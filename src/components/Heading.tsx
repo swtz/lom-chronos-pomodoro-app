caa88127637f80cd8ee0f84f598938eafda29b70
Leonardo Lüders <luderser@hotmail.com>
2025-04-18 17:06:21 -0300

    Finished class 16 – variables and CSS reset of the Chronos app.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,3 @@
-import styles from './Heading.module.css';
-
-export function Heading({ children, color }) {
-  return <h1 style={{ background: color }}>{children}</h1>;
+export function Heading({ children }) {
+  return <h1>{children}</h1>;
 }

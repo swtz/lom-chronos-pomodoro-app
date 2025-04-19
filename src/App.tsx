19133dc87e6aa60a6e9062aa6d843f22f887cf88
Leonardo Lüders <luderser@hotmail.com>
2025-04-19 12:14:50 -0300

    Finished class 19 – Created the HTML and CSS structure of the app for each section of the site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
-  console.log('Hello, world!');
-
   return (
-    <div>
-      <Heading>
-        Hello, luderser!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non
-        perferendis vero in aperiam quae numquam et! Odit, vel reiciendis
-        repudiandae iste provident cupiditate repellendus, voluptate facilis in
-        officia quo voluptas!
-      </p>
-    </div>
+    <>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
   );
 }

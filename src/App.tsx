30413396730040ad6bd06a65bfd2e3226a3f17e7
Leonardo Lüders <luderser@hotmail.com>
2025-04-19 12:32:28 -0300

    Finished class 20 – Moved the Container into its own React Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Testing Heading Component</Heading>
+      </Container>
+      <Container>
+        <Heading>Testing Heading Component</Heading>
+      </Container>
+      <Container>
+        <Heading>Testing Heading Component</Heading>
+      </Container>
+      <Container>
+        <Heading>Testing Heading Component</Heading>
+      </Container>
     </>
   );
 }

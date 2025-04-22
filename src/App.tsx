6aa9b3947e904980107cbbccc57f74e524ed45b2
Leonardo Lüders <luderser@hotmail.com>
2025-04-22 11:16:33 -0300

    Finished class 31 – Created the Footer component.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -48,6 +49,10 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href='#'>Entendo como funciona a técnica pomodoro 🍅</a>
+      <a href='#'>
+        Chronos Pomodoro &copy; {new Date().getFullYear()} — Feito com ❤️
+      </a>
+    </footer>
+  );
+}

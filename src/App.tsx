75ab5f90b7005169a0a6f601f3a9bce116626a40
Leonardo Lüders <luderser@hotmail.com>
2025-04-21 22:15:00 -0300

    Finished class 22 – Created the Logo component with icon and link.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Testing Heading Component</Heading>
-      </Container>
-      <Container>
-        <Heading>Testing Heading Component</Heading>
-      </Container>
-      <Container>
-        <Heading>Testing Heading Component</Heading>
-      </Container>
-      <Container>
-        <Heading>Testing Heading Component</Heading>
+        <Logo />
       </Container>
     </>
   );

d8484341779abf0ab78010a3548bddf24ae16bab
Leonardo Lüders <luderser@hotmail.com>
2025-04-22 11:05:15 -0300

    Finished class 30 – Created a reusable Button component with config options.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import './styles/global.css';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function App() {
   return (
@@ -43,7 +45,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>

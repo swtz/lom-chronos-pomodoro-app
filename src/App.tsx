6f1845f06b3c5faa69374c0bdf09d6e3521e1de4
Leonardo Lüders <luderser@hotmail.com>
2025-04-21 22:26:18 -0300

    Finished class 23 – Created Menu component with icons and links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -9,6 +10,9 @@ export function App() {
     <>
       <Container>
         <Logo />
+      </Container>
+      <Container>
+        <Menu />
       </Container>
     </>
   );

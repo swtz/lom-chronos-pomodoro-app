aa755442c9ed5d1b024027772bddcd0c131b990f
Leonardo Lüders <luderser@hotmail.com>
2025-04-24 11:35:15 -0300

    Finished class 44 — Prop drilling: passing state through multiple component levels.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,34 +4,16 @@ import { MainForm } from '../../components/MainForm';
 import { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-type HomeProps = {
+export type HomeProps = {
   state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 };
 
 export function Home(props: HomeProps) {
-  const { state, setState } = props;
-
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          shortBreakTime: 14,
-        },
-      };
-    });
-  }
-
   return (
     <MainTemplate>
       <Container>
-        <button onClick={handleClick}>Clique-me!</button>
-      </Container>
-
-      <Container>
-        <CountDown />
+        <CountDown {...props} />
       </Container>
 
       <Container>

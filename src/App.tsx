4155e9bad51d97be20ae2f21c296facee310a8c7
Leonardo Lüders <luderser@hotmail.com>
2025-04-24 11:03:50 -0300

    Finished class 43 — Global application state with useState and communication via props.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+import { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
+// export type TaskStateModel = {
+//   tasks: TaskModel[];
+//   secondsRemaining: number;
+//   formattedSecondsRemaining: string;
+//   activeTask: TaskModel | null;
+//   currentCycle: number; // número de 1 a 8
+//   config: {
+//     workTime: number;
+//     shortBreakTime: number;
+//     longBreakTime: number;
+//   };
+// };
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+  console.log('APP', state);
+
+  return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,35 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+  const { state, setState } = props;
+
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          shortBreakTime: 14,
+        },
+      };
+    });
+  }
+
   return (
     <MainTemplate>
+      <Container>
+        <button onClick={handleClick}>Clique-me!</button>
+      </Container>
+
       <Container>
         <CountDown />
       </Container>

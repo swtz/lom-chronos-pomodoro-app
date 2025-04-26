187c380ec161ff8c918a7f6d6a56204882faa466
Leonardo Lüders <luderser@hotmail.com>
2025-04-26 13:10:34 -0300

    Finished class 63 — Understood useReducer, reducer, dispatch and actions to work with state.

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { TaskContext } from './TaskContext';
 import initialState from './initialTaskState';
 
@@ -9,13 +9,31 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialState);
 
-  useEffect(() => {
-    console.log('State changed:', state);
-  }, [state]);
+  const [number, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+    switch (action) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      case 'CLEAN':
+        return 0;
+    }
+
+    return state;
+  }, 0);
+
+  // useEffect(() => {
+  //   console.log('State changed:', state);
+  // }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>The number is: {number}</h1>
+      <button onClick={() => dispatch('INCREMENT')}>Increment</button>
+      <button onClick={() => dispatch('DECREMENT')}>Decrement</button>
+      <button onClick={() => dispatch('CLEAN')}>Clean</button>
     </TaskContext.Provider>
   );
 }

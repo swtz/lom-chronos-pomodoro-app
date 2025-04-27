99b302e9c2bee78b03aa56fb39631a2cd9f3e815
Leonardo Lüders <luderser@hotmail.com>
2025-04-27 18:34:58 -0300

    Finished class 71 — Web Worker with setTimeout and useEffect for the countdown feature.

--- src/workers/timerWorker.js
+++ src/workers/timerWorker.js
@@ -1,1 +1,28 @@
-self.onmessage = function (event) {};
+let isRunning = false;
+
+self.onmessage = function (event) {
+  if (isRunning) return;
+
+  isRunning = true;
+
+  const state = event.data;
+  const { activeTask, secondsRemaining } = state;
+
+  // secondsRemaining é convertido de segundos para milissegundos
+  // endDate é a soma dos milissegundos iniciais mais os milissegundos definido pelo usuário
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
+  // garantido que valor não comece com 1 segundo a menos
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+
+  function tick() {
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    // obtém-se os segundos restantes a cada chamada da função
+    countDownSeconds = Math.floor((endDate - now) / 1000);
+
+    setTimeout(tick, 1000); // esse loop será controlado no 'Worker' do componente
+  }
+  tick();
+};

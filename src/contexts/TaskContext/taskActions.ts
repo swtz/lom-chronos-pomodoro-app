7d8bd28bc34a7687112cb32329959d32dae03163
Leonardo Lüders <luderser@hotmail.com>
2025-04-27 19:02:59 -0300

    Finished class 72 — Updating the state and page counter every second.

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -4,6 +4,8 @@ export enum TaskActionTypes {
   START_TASK = 'START_TASK',
   INTERRUPT_TASK = 'INTERRUPT_TASK',
   RESET_STATE = 'RESET_STATE',
+  COUNT_DOWN = 'COUNT_DOWN',
+  COMPLETE_TASK = 'COMPLETE_TASK',
 }
 
 export type TaskActionsModel =
@@ -12,8 +14,15 @@ export type TaskActionsModel =
       payload: TaskModel;
     }
   | {
+      type: TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    }
+  | {
       type: TaskActionTypes.INTERRUPT_TASK;
     }
   | {
       type: TaskActionTypes.RESET_STATE;
+    }
+  | {
+      type: TaskActionTypes.COMPLETE_TASK;
     };

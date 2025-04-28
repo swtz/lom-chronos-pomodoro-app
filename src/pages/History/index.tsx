1159b0ed7d537657d38964caad02ce88a9eaf3d6
Leonardo Lüders <luderser@hotmail.com>
2025-04-28 19:55:42 -0300

    Finished class 83 — Formatted the date with date-fns package.

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,6 +6,7 @@ import { MainTemplate } from '../../templates/MainTemplate';
 
 import styles from './styles.module.css';
 import { useTaskContext } from '../../hooks/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
 
 export function History() {
   const { state } = useTaskContext();
@@ -50,7 +51,7 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{formatDate(task.startDate)}</td>
                     <td>{task.interruptDate}</td>
                     <td>{taskType[task.type]}</td>
                   </tr>

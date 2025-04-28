67543b64a452d288841fe542d18d490f42d6942f
Leonardo Lüders <luderser@hotmail.com>
2025-04-28 11:51:49 -0300

    Finished class 77 — React Router: created the Router and ScrollToTop components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { TaskContextProvider } from './contexts/TaskContext';
-import { Home } from './pages/Home';
+import { MessagesContainer } from './components/MessagesContainer';
+import { MainRouter } from './routers/MainRouter';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { MessagesContainer } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro' element={<AboutPomodoro />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
